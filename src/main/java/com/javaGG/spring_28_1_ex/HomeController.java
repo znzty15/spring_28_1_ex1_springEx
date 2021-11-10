2b6280210927ce2f272db83bb9b378c9aa389fd3
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-11-10 15:03:22 +0900

    MyBatis Ex v1.0

---
+++ src/main/java/com/javaGG/spring_28_1_ex/HomeController.java
@@ -0,0 +1,79 @@
+package com.javaGG.spring_28_1_ex;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaGG.spring_28_1_ex.dao.ContentDao;
+import com.javaGG.spring_28_1_ex.dto.ContentDto;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	ContentDao dao;
+	
+	@Autowired
+	public void setDao(ContentDao dao) {
+		this.dao = dao;
+	}
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		ArrayList<ContentDto> dtos = dao.listDao();
+		model.addAllAttributes(dtos);
+		return "list";
+	}
+	
+	@RequestMapping("/writeForm")
+	public String writeForm(Model model) {
+		return "writeForm";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(HttpServletRequest request, Model model) {
+		
+		dao.deleteDao(request.getParameter("mId"));
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/write")
+	public String write(HttpServletRequest request, Model model) {
+		dao.writeDao(request.getParameter("mWriter"), request.getParameter("mContent"));
+		return "redirect:list";
+	}
+	
+}
